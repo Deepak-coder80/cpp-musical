f6325a074c425bc780d9ac294e2e24f4c1a396bf
Deepak M S <souparnikad40@gmail.com>
2021-10-10 11:40:47 +0530

    finished

--- main.cpp
+++ main.cpp
@@ -4,10 +4,19 @@
 
 int main(){
 
+    int age =18;
+    std::string name = "Deepak";
+    float percentage=94.6;
+    double mark = 236;
+    char grade = 'A';
+
     //cout - console out 
     //endl - end the line
    std::cout << "Hello World !"<<std::endl;
-   std::cout << "Hai, My name is Deepak"<<std::endl;
+   std::cout << "Hai, My name is "<<name<<std::endl;
+   std::cout <<"Age is "<<age<<std::endl;
+   std::cout <<"I have scored "<<mark <<" In An exam,"<<std::endl;
+   std::cout << "With Percentage "<<percentage<<" and Grade "<<grade<<std::endl;
    // cout << "Hello World !"<<endl;
     return 0;
 }
