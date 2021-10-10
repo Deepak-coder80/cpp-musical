f43cd1c4544d66def21a24839f5d90b07d062756
Deepak M S <souparnikad40@gmail.com>
2021-10-10 11:13:13 +0530

    hello world programe finished

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+//using namespace std;
+
+int main(){
+
+    //cout - console out 
+    //endl - end the line
+   std::cout << "Hello World !"<<std::endl;
+   std::cout << "Hai, My name is Deepak"<<std::endl;
+   // cout << "Hello World !"<<endl;
+    return 0;
+}
