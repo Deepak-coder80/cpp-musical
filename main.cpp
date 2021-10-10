a7827bd7fa21a65d6b173718408801f948e1054f
Deepak M S <souparnikad40@gmail.com>
2021-10-10 12:13:31 +0530

    finished

--- main.cpp
+++ main.cpp
@@ -1,27 +1,52 @@
 #include <iostream>
 
-//using namespace std;
+using namespace std;
 
 int main(){
 
-    int i=0;
-    std::string name = "Deepak";
-    
-    // std::cout<<name.length()<<std::endl;
+    //step1: declare the variables
+    float num1,num2;
+    int option,choice;
 
-    // for(i=0;name[i]!='\0';i++){
+   do
+   {
+        //step 2: get the option
+        cout<<"Enter the operation: \n\t Enter 1 for Addition "<<endl;
+        cout<<"\t Enter 2 for Substraction \n\t Enter 3 for Product "<<endl;
+        cout<<"\t Enter 4 for Division\n Enter the option here:";
+        cin>>option;
 
-    //      std::cout<<name[i]<<std::endl;
-    // }
+        //step 3: get the numbers
+        cout<<"Enter the first Number:";
+        cin>>num1;
+        cout<<"Enter the second Number:";
+        cin>>num2;
 
-    //std::cout<<name.find("pak",0); //--->for finding a char or string 
-    //an string . 2 parameter string or char and starting index
 
-    //for searching substring.
-    //also takes two parameters where to start and how many character
-    std::string subName = name.substr(3,3);
-    std::cout<< subName;
+        //step 4 : goto correct calculation
+        switch (option)
+        {
+        case 1:
+            cout<<num1<<" + "<<num2<<" = "<<(num1+num2)<<endl;
+            break;
+        case 2:
+            cout<<num1<<" - "<<num2<<" = "<<(num1-num2)<<endl;
+            break;
+        case 3:
+            cout<<num1<<" x "<<num2<<" = "<<(num1*num2)<<endl;
+            break;
+        case 4:
+            cout<<num1<<" / "<<num2<<" = "<<(num1/num2)<<endl;
+            break;
+        
+        default:
+            cout<<"Invalid Operation"<<endl;
+            break;
+        }
+       cout<<"Do you want to conitinue? if yes enter 1:";
+       cin>> choice;
+   } while (choice==1);
+   
 
-    
     return 0;
 }
